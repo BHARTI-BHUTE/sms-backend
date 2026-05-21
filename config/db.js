5bb92230d321f5fda3455116aeebe20fcc0ec7b3
unknown <mangeshbhute16@gmail.com>
2026-05-22 03:09:41 +0530

    remove dotenv from production, use dotenv-cli for dev

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 // after
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
 
 
 console.log('DB_HOST:', process.env.DB_HOST);
