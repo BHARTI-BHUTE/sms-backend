8deac57b855ccfbf269a54aadfacbadcd86d9b0f
unknown <mangeshbhute16@gmail.com>
2026-05-22 01:30:13 +0530

    "Use railway mysql url"

--- config/db.js
+++ config/db.js
@@ -1,28 +1,8 @@
 require('dotenv').config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
-  connectTimeout: 60000,
-
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 pool.getConnection()
   .then(conn => {
